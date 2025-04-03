a498459aa34defa417421de35f3c1c2e73a25726
Emily Pan <contact.emilypan@gmail.com>
2025-04-02 23:17:49 -0700

    fixed issue: script froze hammerspoon
    CAUSE: 
    used hs.execute for afplay
    
    EFFECT: 
    hammerspoon and script would freeze when no audio was playing (so silent audio needed to be played) because afplay keeps running until the audio file ends (we all would be stuck for an hour until it unfroze ;-; maybe)
    
    FIX: 
    used hs.task to let the code simultaneously play the audio while running the rest of the code

--- init.lua
+++ init.lua
@@ -1,18 +1,45 @@
+done = false
 hs.notify.withdrawAll()
+hs.execute("pkill afplay")
 
-debugMode = hs.dialog.blockAlert("Do you want to enable debug mode?", "If Yes, you will get notifications for every successful step and error. If No, you'll only get notifications for errors.", "Yes", "No")
+
+hs.alert.show("🎁 Hooray! Welcome to emilypancake's first big project on GitHub!")
 
 
-notify = hs.notify.new({
-    title = "🎁  Anti Pop/Crackle app opening!", 
-    informativeText = "Hooray! Welcome to my first big project on GitHub! Press CMD + SHIFT + 9 to quit.",
-    withdrawAfter = 0
-}):send()
+hs.timer.doAfter(2.1, function()
+    hs.alert.show("⚠️ If you have Do not Disturb enabled then") 
+end)
+
+hs.timer.doAfter(4.2, function()
+    hs.alert.show("⚠️ without notifications, errors or debug mode won't work")
+end)
+
+hs.timer.doAfter(6.3, function()
+    hs.alert.show("✅ The script will still work without notifications")
+end)
+
+hs.timer.doAfter(8.4, function()
+    hs.alert.show("Press CMD + SHIFT + 9 to quit script.")
+end)
+
+hs.timer.doAfter(10.5, function()
+    hs.alert.show("Open Hammerspoon to start.")
+end)
+
+hs.timer.doAfter(12.6, function()
+    hs.alert.show("For setup, visit popperstopper Github repository.")
+end)
+
+debugMode = hs.dialog.blockAlert("Do you want to enable debug mode?", "If Yes, you will get notifications for every successful step and error. \n \n If No [recommended], you'll only get notifications for errors.", "Yes", "No")
+
+
+
+
 
 hs.hotkey.bind({"cmd", "shift"}, "9", function()
     exitScript = hs.dialog.blockAlert("Do you want to exit the Anti Pop/Crackle script?", "Yes to exit, No to stay.", "Yes", "No")
     if exitScript == "Yes" then
-        os.exit()
+        return
     end
 end)
 
@@ -86,21 +113,38 @@ else
 end
 
 function checkAndPlaySilentAudio()
-    
+    if debugMode == "Yes" then
+        hs.alert.show(audioDevice:name());
+    end
     if string.match(string.lower(audioDevice:name()), "headphones") then  -- this only works if your audio device name contains headphones
-        local output = hs.execute("pgrep afplay")   -- grep is like control f 
-        output = output and output:gsub("%s+", "") 
-
-        if output == nil or output == "" then 
-            hs.execute("afplay " .. audioFilePath) 
+        output = hs.execute("pgrep afplay") -- finds audio
+        if output == "" or output == nil then 
+            if debugMode == "Yes" then
+                hs.notify.new({
+                    title = "✅  Noticed audio is silent",
+                    informativeText = "Attempting to enable anti pop!",
+                    withdrawAfter = 0
+                }):send()
+            end
+            hs.task.new("/usr/bin/afplay", function(exitCode, standardOutput, standardError)
+                hs.notify.new({
+                    title = "⚠️ afplay issue",
+                    informativeText = "Exit Code: ".. exitCode.. ", Out: ".. standardOutput .. "ErrorOut: " .. standardError,
+                    withdrawAfter = 0
+                }):send()
+                
+            end , {audioFilePath}):start()
         end
     else
         hs.execute("pkill afplay") 
     end
 end
 
-hs.timer.doEvery(60, function()
+
+checkAndPlaySilentAudio() 
+hs.timer.doEvery(3600, function()
     checkAndPlaySilentAudio() 
 end)
-checkAndPlaySilentAudio()
+
+
 
